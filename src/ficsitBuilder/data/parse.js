8348d3d47545955e9263d1e17c30d967fa581241
Daniil Zagumennii <danielzagumennyi@gmail.com>
2023-09-15 13:19:52 +0300

    update

--- src/ficsitBuilder/data/parse.js
+++
@@ -1,7 +0,0 @@
-import parseDocs from "satisfactory-docs-parser";
-import fs from "fs";
-
-const file = fs.readFileSync("./src/ficsitBuilder/data/Docs.json");
-const data = parseDocs(file);
-
-fs.writeFileSync("./src/ficsitBuilder/data/parsedData.json", JSON.stringify(data))
